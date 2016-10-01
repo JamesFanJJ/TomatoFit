d82053a8e7ec3876830d76680fded18ff203e82e
JamesFanJJ <fanjunjie@Fans-MBP-2.fios-router.home>
2016-09-30 20:53:38 -0400

    App Created

---
+++ TomatoFitApp/app/src/main/java/com/fanjunjie/tomatofitapp/MainTomatoFit.java
@@ -0,0 +1,13 @@
+package com.fanjunjie.tomatofitapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainTomatoFit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_tomato_fit);
+    }
+}
